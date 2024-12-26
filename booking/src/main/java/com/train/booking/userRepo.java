ce28ce5c0559c6f1bc3c904e6aa172e1791ea13c
Atul Kumar <123546551+mohitatulkarn@users.noreply.github.com>
2024-12-26 14:48:47 +0530

    Delete booking directory

--- booking/src/main/java/com/train/booking/userRepo.java
+++
@@ -1,7 +0,0 @@
-package com.train.booking;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface userRepo extends JpaRepository<USER, Long> {
-       USER findByUsername(String username);
-}
